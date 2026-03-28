4bd04085ea9baa69f3c67ea007f8d05738f36ff0
antmerro <antoniodavidmartinezsanchez@gmail.com>
2026-03-28 17:16:49 +0100

    fix(jest): add TextEncoder polyfill and --forceExit flag

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,1 +1,6 @@
 import '@testing-library/jest-dom';
+import { TextEncoder, TextDecoder } from 'util';
+
+// jsdom does not expose TextEncoder/TextDecoder globally; Prisma's internals require them
+global.TextEncoder = TextEncoder;
+global.TextDecoder = TextDecoder as typeof global.TextDecoder;
